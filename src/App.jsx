93a31f9ab3a80b736889245107f003698f032d57
RosaBen <benchabane.rosa@outlook.com>
2024-07-01 10:17:03 +0200

    basic routes for about and home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import Home from "./pages/Home";
-import About from "./pages/About";
+import { useState } from "react";
+
 import "./App.css";
 
+// page components
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+// the App
+
 function App() {
+  const [currentLocation, setCurrentLocation] = useState("/");
+
   return (
     <>
-      <Home />
-      <About />
+      <nav>
+        <button onClick={() => setCurrentLocation("/")} type="button">
+          Home
+        </button>
+        <button onClick={() => setCurrentLocation("/about")} type="button">
+          About
+        </button>
+      </nav>
+      <main>
+        {currentLocation === "/" && <Home />}
+        {currentLocation === "/about" && <About />}
+      </main>
     </>
   );
 }
