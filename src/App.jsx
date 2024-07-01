a4698d9b6d9a36b9c1358edd645cc3acad794899
RosaBen <benchabane.rosa@outlook.com>
2024-07-01 10:11:31 +0200

    created about and home pages

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
+  );
+}
+
+export default App;
